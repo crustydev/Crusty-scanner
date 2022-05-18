a74b684a2a2ed95a9ed6f0689c0a04121d2c68c7
Ademola <ademola1145@gmail.com>
2022-05-18 17:44:07 +0100

    remove cve scanners

--- src/scanners/mod.rs
+++ src/scanners/mod.rs
@@ -1,21 +1,8 @@
 pub mod traits;
-use traits::CveScanner;
 use traits::SubdomainScanner;
 pub mod models;
 
 mod subdomains;
-mod cves;
-
-
-pub fn cve_scanners() -> Vec<Box<dyn CveScanner>> {
-    return vec![
-        Box::new(cves::DotEnvScan::new()),
-        Box::new(cves::DsStoreScan::new()),
-        Box::new(cves::ElasticsearchScan::new()),
-        Box::new(cves::GitlabOpenScan::new()),
-        Box::new(cves::PrometheusDashboardScan::new())
-    ]
-}
 
 
 // Can't get webarchive scan to work for some reason
@@ -25,15 +12,7 @@ pub fn subdomain_scanners() -> Vec<Box<dyn SubdomainScanner>> {
         Box::new(subdomains::CrtShScan::new()),
         Box::new(subdomains::ThreatCrowdScan::new()),
         Box::new(subdomains::ThreatMinerScan::new()),
-        //Box::new(subdomains::WebArchiveScan::new())
+        //Box::new(subdomains::WebArchiveScan::new()),
     ];
 }
 
-#[derive(Debug, Clone)]
-pub enum CveScanResult {
-    DotEnvFileDisclosure(String),
-    DSStoreFileDisclosure(String),
-    ElasticsearchUnauthenticatedAccess(String),
-    GitlabOpenRegistration(String),
-    PrometheusOpenDashboard(String)
-}
