351c7d06a5ebe10ac06566232e0e91c20930ee82
Ademola <ademola1145@gmail.com>
2022-05-18 17:43:12 +0100

    remove vulnerability search feature

--- src/core.rs
+++ src/core.rs
@@ -1,30 +1,20 @@
 use futures::{stream, StreamExt};
-use reqwest::Client;
 
 use std::collections::HashSet;
 use std::iter::FromIterator;
-use std::time::{Duration, Instant};
+use std::time::Instant;
 
 use crate::utils;
 use crate::utils::Error;
 
 use crate::scanners;
-use crate::scanners::traits::CveScanner;
 use crate::scanners::models::Subdomain;
 
 pub fn list_scanners() {
-    let cve_scanners = scanners::cve_scanners();
     let subdomain_scanners = scanners::subdomain_scanners();
 
     println!("\nSubdomain scanners:");
     for scanner in subdomain_scanners {
-        println!("*    {}\n\t{}", scanner.name(), scanner.about());
-    }
-
-    println!("\n__________________________________________________________________________________________________________________");
-
-    println!("\nCVE scanners:");
-    for scanner in cve_scanners {
         println!("*    {}\n\t{}", scanner.name(), scanner.about());
     }
 }
@@ -34,7 +24,7 @@ pub fn about() {
     println!("Authored by: Fatoke Ademola Paul(crusty dev)");
     println!("Written in: The Rust Programming Language");
     println!("Inspiration drawn from: tricoder by Sylvain Kerkour");
-    println!("What it does: Scans a target domain and its subdomains for vulnerabilities");
+    println!("What it does: Scans a target domain for its subdomains and their open ports");
     println!("Enjoy using!");
 }
 
@@ -54,14 +44,12 @@ pub fn scan(target: &str) -> Result<(), Error> {
         .build()
         .expect("Building tokio's runtime");
 
-    let http_timeout = Duration::from_secs(10);
-    let http_client = Client::builder().timeout(http_timeout).build()?;
     let dns_resolver = utils::new_dns_resolver();
 
     let subdomains_concurrency = 100;
     let dns_concurrency = 200;
     let ports_concurrency = 200;
-    //let vulnerabilities_concurrency = 20;
+    
     let scan_start_time = Instant::now();
 
     let subdomains_modules = scanners::subdomain_scanners();
@@ -132,37 +120,6 @@ pub fn scan(target: &str) -> Result<(), Error> {
             }
         }
 
-
-        /*
-        println!("---------------------- Vulnerabilities ----------------------");
-
-        // Scan each socket address for vulnerabilities
-        // 
-
-        let mut targets: Vec<(Box<dyn CveScanner>, String)> = Vec::new();
-        for subdomain in subdomains {
-            for port in subdomain.open_ports {
-                let cve_scanners = scanners::cve_scanners();
-                for cve_scanner in cve_scanners {
-                    let target = format!("http://{}:{}", &subdomain.domain_name, port.port);
-                    targets.push((cve_scanner, target));
-                }
-            }
-        }
-
-        stream::iter(targets.into_iter())
-            .for_each_concurrent(vulnerabilities_concurrency, |(scanner, target)| {
-                let http_client = http_client.clone();
-                async move {
-                    match scanner.scan(&http_client, &target).await {
-                        Ok(Some(finding)) => println!("{:?}", &finding),
-                        Ok(None) => {}
-                        Err(err) => log::debug!("Error: {}", err),
-                    };
-                }
-            })
-            .await;
-        */
     });
 
     let scan_duration = scan_start_time.elapsed();
